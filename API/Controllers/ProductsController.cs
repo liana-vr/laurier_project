257c38c6e33cb47d82b7a6676de8442099276532
lianatime <liana.vanrensburg@gmail.com>
2021-03-10 19:13:03 +1300

    Error Handling and Middleware

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,61 +1,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.DTOs;
-using AutoMapper;
-using Core.Entities;
-using Core.Interfaces;
-using Core.Specifications;
 using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+using Core.Specifications;
+using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
+using API.DTOs;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
-        private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
         private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IGenericRepository<Product> _productsRepo;
         private readonly IMapper _mapper;
+
         public ProductsController(IGenericRepository<Product> productsRepo,
-        IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo,
-        IMapper mapper)
+            IGenericRepository<ProductType> productTypeRepo,
+            IGenericRepository<ProductBrand> productBrandRepo, 
+            IMapper mapper)
         {
             _mapper = mapper;
+            _productsRepo = productsRepo;
             _productTypeRepo = productTypeRepo;
             _productBrandRepo = productBrandRepo;
-            _productsRepo = productsRepo;
-
         }
 
-    [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
-    {
-        var spec = new ProductsWithTypesAndBrandsSpecification();
-        var products = await _productsRepo.ListAsync(spec);
-        return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
-    }
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification();
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
-    {
-        var spec = new ProductsWithTypesAndBrandsSpecification(id);
-        var product = await _productsRepo.GetEntityWithSpec(spec);
-        return _mapper.Map<Product, ProductToReturnDto>(product);
-    }
+            var products = await _productsRepo.ListAsync(spec);
 
-    [HttpGet("brands")]
-    public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
-    {
-        return Ok(await _productBrandRepo.ListAllAsync());
+            return Ok(_mapper.Map<IReadOnlyList<ProductToReturnDto>>(products));
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+            var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
+
+            return _mapper.Map<ProductToReturnDto>(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetBrands()
+        {
+            return Ok(await _productBrandRepo.ListAllAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetTypes()
+        {
+            return Ok(await _productTypeRepo.ListAllAsync());
+        }
     }
-    [HttpGet("types")]
-    public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
-    {
-        return Ok(await _productTypeRepo.ListAllAsync());
-    }
-}
 }
---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Oh no! This is a Bad Request 💔",
+                401 => "Unauthorized 🚫",
+                404 => "We can't find it anywhere 😭",
+                500 => "An Error occured 👾",
+                _ => null
+            };
+        }
+    }
+}
