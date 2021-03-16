7121c65801ffa6ee9fb67d88fba98771993b32ca
lianatime <liana.vanrensburg@gmail.com>
2021-03-16 19:26:05 +1300

    Validation Added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -84,6 +84,11 @@ namespace API.Controllers
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        if(CheckEmailExistsAsync(registerDto.Email).Result.Value)
+        {
+            return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new [] {"Email Address Already In Use"}});
+        }
+
         var user = new AppUser
         {
             DisplayName = registerDto.DisplayName,
